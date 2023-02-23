7d16cb19985d8a3d6d2f2be1d5f2396462e2c248
David Ricketts <david.ricketts119@gmail.com>
2023-02-23 06:07:32 -0500

    ReactRouter Home page

---
+++ src/pages/Homepage.js
@@ -0,0 +1,11 @@
+import { Container } from "reactstrap";
+
+const HomePage = () => {
+    return (
+        <Container>
+            <h1> Home page.</h1>
+        </Container>
+    )
+}
+
+export default HomePage;
