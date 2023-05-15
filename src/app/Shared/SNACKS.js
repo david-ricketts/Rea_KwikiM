154a3d4577a14262fd3076e43f9387330d97058c
David Ricketts <david.ricketts119@gmail.com>
2023-05-15 10:18:19 -0400

    Snacks arr

---
+++ src/app/Shared/SNACKS.js
@@ -0,0 +1,41 @@
+import chipImg from '../assets/img/Chip.png';
+import cookieImg from '../assets/img/Cookies.jpg';
+import fryImg from '../assets/img/Fries.jpg';
+import muffinImg from '../assets/img/redux-woods.jpg';
+
+export const SNACKS = [
+    {
+        id: 0,
+        name: 'Chips',
+        image: chipImg,
+        quantity: 12,
+        description:
+            'Crispy, crunchy, and delicious'
+    },
+    {
+        id: 1,
+        name: 'Cookies',
+        image: cookieImg,
+        quantity: 12,
+        description:
+            'Warm, chewy, and melting'
+    },
+    {
+        id: 2,
+        name: 'Fries',
+        image: fryImg,
+        elevation: 2901,
+        quantity: 13,
+        description:
+            'Hot, crisp, fluffy'
+    },
+    {
+        id: 3,
+        name: 'f',
+        image: muffinImg,
+        elevation: 42,
+        featured: true,
+        description:
+            "You'll never want to leave this hidden gem, deep within the lush Redux Woods."
+    }
+];
