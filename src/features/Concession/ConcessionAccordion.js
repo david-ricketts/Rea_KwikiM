64a87c4baa3f46845b6f04b31c82d6b3714a05a0
David Ricketts <david.ricketts119@gmail.com>
2023-05-16 08:05:29 -0400

    ConcessionsList start

--- src/features/Concession/ConcessionAccordion.js
+++ src/features/Concession/ConcessionAccordion.js
@@ -23,11 +23,7 @@ const ConcessionsAccordion = () => {
             <AccordionItem>
                 <AccordionHeader targetId="1">Accordion Item 1</AccordionHeader>
                     <AccordionBody accordionId="1">
-                        <strong>This is the first item&#39;s accordion body.</strong>
-                        You can modify any of this with custom CSS or overriding our default
-                        variables. It&#39;s also worth noting that just about any HTML can
-                        go within the <code>.accordion-body</code>, though the transition
-                        does limit overflow.
+                        <ConcessionsList meals={meals}/>
                     </AccordionBody>
                 </AccordionItem>
             <AccordionItem>
---
+++ src/features/Concession/ConcessionsList.js
@@ -0,0 +1,1 @@
+import { Col, Row } from "reactstrap";
