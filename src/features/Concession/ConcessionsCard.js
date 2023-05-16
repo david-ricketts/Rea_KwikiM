586a6d2872ea8f22834306b93770149152a1bde2
David Ricketts <david.ricketts119@gmail.com>
2023-05-16 14:52:07 -0400

    Working on ConcessionsCard

---
+++ src/features/Concession/ConcessionsCard.js
@@ -0,0 +1,7 @@
+import {Card, CardImg, CardImgOverlay, CardTitle} from 'reactstrap';
+
+
+
+const ConcessionsCard = ({item}) => {
+    const {id, image, name, description} = item;
+}
--- src/features/Concession/ConcessionsList.js
+++ src/features/Concession/ConcessionsList.js
@@ -1,1 +1,22 @@
 import { Col, Row } from "reactstrap";
+
+
+const ConcessionsList = (props) => {
+    const { section } = props;
+    console.log({section})
+
+    return (
+        <Row className="ms-auto">
+            {section.map((item) => {
+                return (
+                    <Col md='4' key={item.id}>
+                        <ConsessionCard item={item} />
+                    </Col>
+                )
+            })}
+        </Row>
+    )
+}
+
+
+export default ConcessionsList;
