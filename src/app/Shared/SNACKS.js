e700a397fd06369003f079a13352a30988a939e8
David Ricketts <david.ricketts119@gmail.com>
2023-05-18 06:49:34 -0400

    Imported images

--- src/app/Shared/SNACKS.js
+++ src/app/Shared/SNACKS.js
@@ -2,8 +2,51 @@ import chipImg from '../assets/img/Chip.png';
 import cookieImg from '../assets/img/Cookies.jpg';
 import fryImg from '../assets/img/Fries.jpg';
 import muffinImg from '../assets/img/Muffin.jpg';
+import burgerImg from '../assets/img/Burger.jpg'
+import paniniImg from '../assets/img/Panini.jpg'
+import ultimateImg from '../assets/img/Ultimate_S.jpg'
+import wingsImg from '../assets/img/Wings.jpg'
+import saladImg from'../assets/img/Salad.jpg'
+import pizzaImg from '../assets/img/pexels-cottonbro-3944315.jpg'
+import appleImg from '../assets/img/Apples.png'
+import brownieImg from '../assets/img/Brownie.jpg'
+import cokeImg from '../assets/img/coke'
 
 export const SNACKS = [
+    {
+        id: 0,
+        name: 'Burger',
+        image: chipImg,
+        quantity: 12,
+        description:
+            'Hot, juicy, and beefy'
+    },
+    {
+        id: 1,
+        name: 'Cookies',
+        image: cookieImg,
+        quantity: 12,
+        description:
+            'Warm, chewy, and melting'
+    },
+    {
+        id: 2,
+        name: 'Fries',
+        image: fryImg,
+        elevation: 2901,
+        quantity: 13,
+        description:
+            'Hot, crisp, fluffy'
+    },
+    {
+        id: 3,
+        name: 'Muffin',
+        image: muffinImg,
+        elevation: 42,
+        featured: true,
+        description:
+            "Soft, sweet, and fruity"
+    },
     {
         id: 0,
         name: 'Chips',
